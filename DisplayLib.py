b03574e7c53f8ab9ecbfd1486ef1caf17a9039c7
lanyuan27 <yingyewu27@163.com>
2016-07-21 22:43:32 -0400

    Add display.py

---
+++ DisplayLib.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-  
+  
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(1280, 960))
+class DisplayLib(object):
+    """docstring for Display"""
+    def __init__(self):
+        pass
+
+    ## open virtual dispaly
+    def start_display(self):
+        display.start()
+      
+    ## close virtual dispaly
+    def stop_dispaly(self):
+        display.stop()
